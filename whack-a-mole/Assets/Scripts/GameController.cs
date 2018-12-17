0b6db95b8c90a90604854db38eed7a9dd59c5cab
emily <ydich@college.harvard.edu>
2018-12-17 01:33:14 -0500

    whaaaaack

---
+++ whack-a-mole/Assets/Scripts/GameController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class GameController : MonoBehaviour
+{
+    public GameObject moleContainer;
+    public TextMesh infoText;
+    public Player player; 
+    public float spawnDuration = 1.5f;
+    public float spawnDecrement = 0.1f; 
+    public float minimumSpawnDuration = 0.5f;
+    public float gameTimer = 30f; 
+
+    private Mole[] moles;
+    private float spawnTimer = 0f;
+    private float resetTimer = 3f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        moles = moleContainer.GetComponentsInChildren<Mole>();
+        //Debug.Log(moles.Length); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer -= Time.deltaTime; 
+        if(spawnTimer <= 0f)
+        {
+            int random_mol = Random.Range(0, moles.Length);
+            moles[random_mol].Rise();
+
+            spawnDuration -= spawnDecrement; 
+            if(spawnDuration < minimumSpawnDuration)
+            {
+                spawnDuration = minimumSpawnDuration; 
+            }
+
+            spawnTimer = spawnDuration; 
+        }
+
+        gameTimer -= Time.deltaTime; 
+        if(gameTimer > 0f)
+        {
+            infoText.text = "Hit all the bunnies! \n Time: " + Mathf.Floor(gameTimer) + "\n Score: " + player.score; 
+        } else
+        {
+            infoText.text = "Game over! Your score: " + Mathf.Floor(player.score);
+
+            resetTimer -= Time.deltaTime; 
+            if(resetTimer <= 0f)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+            }
+        }
+        
+    }
+}
---
+++ whack-a-mole/Assets/Scripts/Mole.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mole : MonoBehaviour
+{
+    public float visibleHeight = -0.73f;
+    public float hiddenHeight = -4.25f;
+    public float speed = 4f;
+    public float disappearDuration = 1.25f; 
+
+    private Vector3 targetPosition;
+    private float disappearTimer = 0f; 
+    // Start is called before the first frame update
+    void Awake()
+    {
+        targetPosition = new Vector3(
+                transform.localPosition.x, 
+                hiddenHeight, 
+                transform.localPosition.z
+            );
+        transform.localPosition = targetPosition; 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        disappearTimer -= Time.deltaTime; 
+        if(disappearTimer <= 0f)
+        {
+            Hide(); 
+        }
+        transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, Time.deltaTime*speed); 
+         
+    }
+
+    public void Rise()
+    {
+        targetPosition = new Vector3(
+            transform.localPosition.x,
+            visibleHeight,
+            transform.localPosition.z
+        );
+
+        disappearTimer = disappearDuration; 
+    }
+
+    public void Hide()
+    {
+        targetPosition = new Vector3(
+            transform.localPosition.x,
+            hiddenHeight,
+            transform.localPosition.z
+        );
+    }
+
+    public void OnHit()
+    {
+        Hide(); 
+    }
+}
